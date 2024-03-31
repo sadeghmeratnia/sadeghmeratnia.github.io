d0c8841ad047591590bb93b243f7ad24e2b80334
Mo Sorkhpar <Sorkhpar@outlook.com>
2024-03-31 16:40:41 -0400

    Initial commit.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,14 +2,14 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'msorkhpar' // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -17,162 +17,164 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 4, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
-        },
+          forks: true, // Forked projects will not be displayed if set to true.
+          projects: [] // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        }
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
-      },
+        projects: [] // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      }
     },
     external: {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
-    },
+      projects: []
+    }
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Mo Sorkhpar',
     description: '',
-    imageURL: '',
+    imageURL: ''
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
+    linkedin: 'msorkhpar',
+    twitter: '',
+    mastodon: '',
+    researchGate: 'https://www.researchgate.net/profile/Mo-Sorkhpar',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: '',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'msorkhpar@outlook.com'
   },
   resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: 'https://raw.githubusercontent.com/msorkhpar/msorkhpar.github.io/main/Mo-Sorkhpar-Resume.pdf' // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
+    'Java',
+    'Python',
+    'OOP',
+    'Design Patterns',
+    'Spring Boot',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Kubernetes',
+    'Microservices',
+    'RESTful API',
+    'RabbitMQ',
+    'Apache Kafka',
+    'PostgreSQL',
+    'Orcale DB',
+    'MongoDB',
+    'Neo4j',
+    'Redis',
+    'Hibernate',
+    'JPA',
+    'Maven',
+    'Gradle',
+    'NetowrkX',
+    'Junit',
+    'Mockito',
+    'TestContainers',
+    'Jenkins',
+    'Gitlab CI/CD',
+    'Bitbucket',
+    'Jira',
+    'Git'
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Intellias',
+      position: 'Senior Software Engineer',
+      from: '10/2021',
+      to: '07/2022',
+      companyLink: 'https://intellias.com'
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Routetitan',
+      position: 'Senior Software Engineer',
+      from: '04/2021',
+      to: '10/2021',
+      companyLink: 'https://routetitan.com'
     },
-  ],
-  certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      company: 'Devolon',
+      position: 'Software Developer',
+      from: '02/2020',
+      to: '02/2021',
+      companyLink: 'https://www.devolon.fi'
     },
+    {
+      company: 'Farazpardazan',
+      position: 'Senior Software Engineer',
+      from: '12/2016',
+      to: '01/2020',
+      companyLink: 'https://www.farazpardazan.com'
+    },
+    {
+      company: 'PartGames Studio',
+      position: 'Java Back-end Developer',
+      from: '01/2016',
+      to: '11/2016',
+      companyLink: 'https://www.partgames.com'
+    }
   ],
+  certifications: [],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Indiana State University',
+      degree: 'Master of Science in Computer Science',
+      from: '08/2022',
+      to: '05/2024'
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'University of Mazandaran',
+      degree: 'Bachelor of Engineering in Information Technology',
       from: '2012',
-      to: '2014',
-    },
+      to: '2016'
+    }
   ],
   publications: [
     {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
+      title: 'Vertex Ordering with Precedence Constraints',
+      conferenceName: 'FCT 2023',
       journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
+      authors: 'Jeff Kinne, Akbar Rafiey, Arash Rafiey & Mohammad Sorkhpar (alphabetical order)',
+      link: 'https://doi.org/10.1007/978-3-031-43587-4_22',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
+        'We study bipartite graph ordering problem, which arises in various domains such as production management, bioinformatics, and job scheduling with precedence constraints. In the bipartite vertex ordering problem, we are given a bipartite graph H=(B,S,E) where each vertex in B has a cost and each vertex in S has a profit. The goal is to find a minimum K together with an ordering < of the vertices of H, so that i<j whenever i∈B is adjacent to j∈S . Moreover, at each sub-order the difference between the costs and profits of the vertices in the sub-order does not exceed K.'
+    }
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 0 // How many articles to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-DK8GGHDHQY' // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
     id: '',
-    snippetVersion: 6,
+    snippetVersion: 6
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
@@ -215,7 +217,7 @@ const CONFIG = {
       'dim',
       'nord',
       'sunset',
-      'procyon',
+      'procyon'
     ],
 
     // Custom theme, applied to `procyon` theme
@@ -226,8 +228,8 @@ const CONFIG = {
       neutral: '#2A2730',
       'base-100': '#E3E3ED',
       '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
-    },
+      '--rounded-btn': '3rem'
+    }
   },
 
   // Optional Footer. Supports plain text or HTML.
@@ -236,8 +238,7 @@ const CONFIG = {
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
-
-  enablePWA: true,
+  enablePWA: false
 };
 
 export default CONFIG;
